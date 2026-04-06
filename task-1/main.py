488f2ba5e0a982efe693ac89da1234d22a651900
ananyahegde <ananyahegde533@gmail.com>
2026-04-06 18:49:35 +0530

    add a scheduler for scraping data and exporting to csv

--- task-1/main.py
+++ task-1/main.py
@@ -1,8 +1,12 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import datetime
 from scraper import scrape_all
 from book_parser import parse
-from database import has_todays_prices, create_tables, is_books_empty, insert_books, insert_prices
+from database import is_books_empty, has_todays_prices, insert_books, insert_prices, create_tables
+from reporter import export_csv
 
-if __name__ == "__main__":
+def job():
+    print(f"\n[{datetime.now()}] Starting nightly scrape...")
     create_tables()
     data = parse(scrape_all())
 
@@ -16,4 +20,11 @@ if __name__ == "__main__":
     else:
         print("Today's prices already exist, skipping...")
 
-    print("Done.")
+    export_csv()
+    print(f"[{datetime.now()}] Done.")
+
+if __name__ == "__main__":
+    scheduler = BlockingScheduler()
+    scheduler.add_job(job, "cron", hour=18, minute=47)
+    print("Scheduler started. Waiting for 2:00 AM...")
+    scheduler.start()
