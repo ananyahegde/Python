127b58b678afaaa14a47c3f9f8c544417f5b0ba8
ananyahegde <ananyahegde533@gmail.com>
2026-04-07 10:56:13 +0530

    handle currency symbol in price

--- task-1/book_parser.py
+++ task-1/book_parser.py
@@ -1,14 +1,19 @@
 from bs4 import BeautifulSoup
 import pandas as pd
+import re
 
 def parse(items):
     rows = []
 
     for item in items:
         name = item.find("h3").find("a")["title"]
-        price = item.find("p", class_="price_color").text.strip()
+
+        price_raw = item.find("p", class_="price_color").text.strip()
+        price = float(re.sub(r'[^\d.]', '', price_raw))
+
         sku_raw = item.find("h3").find("a")["href"]
         sku = sku_raw.split("/")[-2]
+
         rows.append({"name": name, "price": price, "sku": sku})
 
     return pd.DataFrame(rows)
